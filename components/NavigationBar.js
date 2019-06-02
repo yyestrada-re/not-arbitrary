61147695dca49345368c174e3e588df10e2ade23
aainav <aainaev887@gmail.com>
2019-06-01 19:36:28 -0500

    navtry4

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,45 +1,14 @@
 import React from "react";
-import {Image, View} from 'react-native';
+import {Text, View} from 'react-native';
 import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
-
-<Image home = {require('../assets/home.png')}> 
-<Image ques = {require('../assets/questions.png')}> 
-<Image recy = {require('../assets/recycle.png')}> 
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> Home! </Text>
-      </View>
-    );
-  }
-}
-
-class QuestionsScreen extends React.Component {
-  render() {
-    return (
-      <View> style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> Questions! </Text>
-      </View>
-    );
-  }
-}
-
-class RecycleScreen extends React.Component {
-  render() {
-    return (
-      <View> style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> Recycle! </Text>
-      </View>
-    );
-  }
-}
+import Home from './components/Home';
+import Effects from './components/Effects';
+import Never from './componenets/Never';
 
 const TabNavigator = createBottomTabNavigator ({
-  home: HomeScreen,
-  ques: QuestionsScreen,
-  recy: RecycleScreen,
+  home: Home,
+  ques: Effects,
+  recy: Never,
 })
 
 export default createAppContainer(TabNavigator)
