94d6aa88ce7353fce07eba2432e7a5a1cf27339c
aainav <aainaev887@gmail.com>
2019-06-01 20:41:59 -0500

    created icons

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,14 +1,31 @@
 import React from "react";
 import {Text, View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
 import Home from './components/Home';
 import Effects from './components/Effects';
 import Never from './componenets/Never';
 
-const TabNavigator = createBottomTabNavigator ({
-  home: Home,
-  ques: Effects,
-  recy: Never,
-})
-
-export default createAppContainer(TabNavigator)
+export default createBottomTabNavigator( 
+  {
+    home: Home,
+    ques: Effects,
+    recy: Never,
+  },
+  {
+    defaultNavigationOptions: ({ navigation}) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor}) => {
+        const { routeName } = navigation.state
+        let icon
+        if (routeName === 'home') {
+          icon = require('../assets/home.png')
+        } else if (routeName === 'ques') {
+          icon = require('../assets/questions.png')
+        } else {
+          icon = require('../assets/recycle')
+        }
+        return <img src={icon}></img>
+      }
+    })
+  }
+)
